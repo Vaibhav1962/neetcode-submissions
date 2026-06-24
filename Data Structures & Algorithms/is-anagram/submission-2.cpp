cd45e011674a411f76fd7d989d23c0b9fd5907ae
Vaibhav Singh <vaibhav.singh.252005@gmail.com>
2026-06-24 22:03:07 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()) return false;
+
+        vector<int>freq(26,0);
+
+        for(auto it : s)
+        {
+            freq[it - 'a']++;
+        }
+        for(auto it : t)
+        {
+            freq[it - 'a']--;
+        }
+        for(auto it : freq)
+        {
+            if(it!=0)
+            return false;
+        }
+
+        return true;
+        
+    }
+};
