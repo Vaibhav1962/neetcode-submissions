8e3ebf0211c7afa4e4b2ff4750401ad13ae0f864
Vaibhav Singh <vaibhav.singh.252005@gmail.com>
2026-06-24 21:53:34 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_map<int,int>mpp;
+
+        for(auto it : nums)
+        {
+            if(mpp[it]==1)
+            {
+                return true;
+            }
+            mpp[it]++;
+        }
+        return false;
+    }
+};
